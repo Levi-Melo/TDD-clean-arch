01782ab460207160b42d737c5825e65b8611baaf
Levi-Melo <levicontat@outlook.com>
2023-01-19 12:30:27 -0300

    ensure Service update account without overriding name

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -62,7 +62,7 @@ describe('FacebookAuthenticationService', () => {
     expect(userAccountRepo.createFromFacebook).toHaveBeenCalledTimes(1)
   })
 
-  it('should call  UpdateFacebookAccountRepo when LoadFacebookUserApi returns undefined', async () => {
+  it('should call UpdateFacebookAccountRepo when LoadFacebookUserApi returns undefined', async () => {
     userAccountRepo.load.mockResolvedValueOnce({
       id: 'any_id',
       name: 'any_name'
@@ -73,6 +73,21 @@ describe('FacebookAuthenticationService', () => {
     expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledWith({
       id: 'any_id',
       name: 'any_name',
+      facebookId: 'any_fb_id'
+    })
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledTimes(1)
+  })
+
+  it('should update account name', async () => {
+    userAccountRepo.load.mockResolvedValueOnce({
+      id: 'any_id'
+    })
+
+    await sut.perform({ token })
+
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledWith({
+      id: 'any_id',
+      name: 'any_fb_name',
       facebookId: 'any_fb_id'
     })
     expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledTimes(1)
