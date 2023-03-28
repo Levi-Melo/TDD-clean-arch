2299af60070e07db38ff79d9542d89992efdea24
Levi-Melo <levicontat@outlook.com>
2023-03-28 11:02:26 -0300

    ensure AccessToken expires in 30 minutes

--- tests/domain/models/access-token.spec.ts
+++ tests/domain/models/access-token.spec.ts
@@ -5,4 +5,8 @@ describe('AccessToken', () => {
     const sut = new AccessToken('any')
     expect(sut).toEqual({ value: 'any' })
   })
+
+  it('should expire in 180000 ms (30 minutes)', () => {
+    expect(AccessToken.expirationInMs).toBe(1800000)
+  })
 })
