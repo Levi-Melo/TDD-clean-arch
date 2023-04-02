60c50b41f49ccd5cdda20772df76ac3e55359986
Levi-Melo <levicontat@outlook.com>
2023-04-02 19:37:42 -0300

    improve code quality

--- src/infra/postgres/repos/user-account-repository.ts
+++ src/infra/postgres/repos/user-account-repository.ts
@@ -3,16 +3,16 @@ import { PgUser } from '../entities'
 import { getRepository } from 'typeorm'
 
 export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
+  private readonly pgUserRepo = getRepository(PgUser)
+
   async load ({ email }: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser)
-    const pgUser = await pgUserRepo.findOne({ where: { email } })
+    const pgUser = await this.pgUserRepo.findOne({ where: { email } })
     if (pgUser != null) { return { id: pgUser.id.toString(), name: pgUser.name ?? undefined } }
   }
 
   async saveWithFacebook ({ email, facebookId, name, id }: SaveFacebookAccountRepository.Params): Promise<SaveFacebookAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser)
     if (id !== undefined) {
-      await pgUserRepo.update({
+      await this.pgUserRepo.update({
         id: Number(id)
       }, {
         facebookId,
@@ -20,7 +20,7 @@ export class PgUserAccountRepository implements LoadUserAccountRepository, SaveF
       })
       return { id }
     }
-    const pgUser = await pgUserRepo.save({
+    const pgUser = await this.pgUserRepo.save({
       email,
       facebookId,
       name
