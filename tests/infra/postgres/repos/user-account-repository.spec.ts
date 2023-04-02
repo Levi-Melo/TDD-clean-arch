c95ec31b0485d73ddb64f232ea43d712bfe49682
Levi-Melo <levicontat@outlook.com>
2023-04-02 19:41:27 -0300

    ensure pgUserAccountRepository return id

--- tests/infra/postgres/repos/user-account-repository.spec.ts
+++ tests/infra/postgres/repos/user-account-repository.spec.ts
@@ -42,13 +42,14 @@ describe('PgUserAccountRepository', () => {
 
   describe('saveWithFacebook', () => {
     it('should create an account if id is undefined', async () => {
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         email: 'any_email',
         name: 'any_name',
         facebookId: 'any_fb_id'
       })
       const pgUser = await pgUserRepo.findOne({ email: 'any_email' })
       expect(pgUser?.id).toBe(1)
+      expect(id).toBe('1')
     })
     it('should update account if id is defined', async () => {
       await pgUserRepo.save({
@@ -57,7 +58,7 @@ describe('PgUserAccountRepository', () => {
         facebookId: 'any_fb_id'
       })
 
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         id: '1',
         email: 'new_email',
         name: 'new_name',
@@ -71,6 +72,7 @@ describe('PgUserAccountRepository', () => {
         name: 'new_name',
         facebookId: 'new_fb_id'
       })
+      expect(id).toBe('1')
     })
   })
 })
